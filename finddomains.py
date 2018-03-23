90d817a422e2e5408fda567de7dec6661b967423
Diego Fellipe Antunes Pedrosa <diego.pedrosa@vitreo.com.br>
2018-03-23 17:29:53 -0300

    Teste

---
+++ finddomains.py
@@ -0,0 +1,40 @@
+import requests
+import json
+import sys
+
+
+def check_dns(ns):
+    r = requests.get('https://dns.coffee/api/nameservers/%s' % ns)
+    result = json.loads(r.text)['data']
+    domains = ('news - %s - %s' % (d['name'], d['firstseen']) for d in result['domains'])
+    archive_domains = ('archive - %s - %s - %s' % (d['name'], d['firstseen'], d['lastseen']) for d in result['archive_domains'])
+    return domains,archive_domains
+
+
+def show_dns(*args):
+    ns = args[0]
+    domains, archive_domains = check_dns(ns)
+    if len(args) > 2:
+        value = args[2]
+        print("\n".join([s for s in domains if value in str(s).lower()]))
+        print("\n".join([s for s in archive_domains if value in str(s).lower()]))
+    else:
+        print("\n".join([s for s in domains]))
+        print("\n".join([s for s in archive_domains]))
+
+
+if __name__ == '__main__':
+    try:
+        show_dns('NS1.ANONS.IO',*sys.argv)
+    except Exception as e:
+        show_dns('NS2.ANONS.IO',*sys.argv)
+    try:
+        show_dns('NS75.DOMAINCONTROL.COM',*sys.argv)
+    except Exception as e:
+        show_dns('NS76.DOMAINCONTROL.COM',*sys.argv)
+    try:
+        show_dns('CARTER.NS.CLOUDFLARE.COM',*sys.argv)
+    except Exception as e:
+        show_dns('TINA.NS.CLOUDFLARE.COM',*sys.argv)
+
+
