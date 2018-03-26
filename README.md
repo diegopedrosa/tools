a64e5462fdcfb7694d9de9ef602a4c8615bbc671
Diego Fellipe Antunes Pedrosa <diego.pedrosa@vitreo.com.br>
2018-03-26 15:41:59 -0300

    Corrigido finddomains

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
-# tools
+# Tools
+Ferramentas para facilitar a o desenvolvimento e automatizar tratamento de incidentes.
 
 * **`checkip`**: Checar geolocalização do endereço ip.
 
+           checkip 8.8.8.8
+           checkip ips.txt
+
 * **`aws`**: ligar, listar e desligar maquinas na aws.
 
+           machines = MachinesAWS(sistema='xyz', ambiente='desenvolvimento')
+           machines.lista  # lista o id das maquinas
+           machines.liga  # liga a lista das listas da maquina
+           machines.desliga  # desliga as maquinas
 
-Ferramentas para facilitar a o desenvolvimento e executar testes de segurança.
+* **`finddomains`**: Achar domínios por string (foco em dominios anonimos).
+   
+           finddomains google
+
+
 
--- finddomains.py
+++ finddomains.py
@@ -1,60 +1,68 @@
 import requests
 import json
 import sys
+from time import sleep
 
 
 def get_zones():
     r = requests.get('https://dns.coffee/api/zones' )
-    return (json.loads(r.text)['data']['zones'])
+    return json.loads(r.text)['data']['zones']
+
 
 def get_ns(zone):
     r = requests.get('https://dns.coffee/api/zones/%s' % zone)
-    return (json.loads(r.text)['data']['nameservers'])
+    return json.loads(r.text)['data']['nameservers']
 
 
 def check_dns(ns):
-    r = requests.get('https://dns.coffee/api/nameservers/%s' % ns)
-    result = json.loads(r.text)['data']
-    domains = ('news - %s - %s' % (d['name'], d['firstseen']) for d in result['domains'])
-    archive_domains = ('archive - %s - %s - %s' % (d['name'], d['firstseen'], d['lastseen']) for d in result['archive_domains'])
+    try:
+        r = requests.get('https://dns.coffee/api/nameservers/%s' % str(ns).lower())
+        result = json.loads(r.text)['data']
+        domains = ('news - %s - %s' % (d['name'], d['firstseen']) for d in result['domains'])
+        archive_domains = ('archive - %s - %s - %s' % (d['name'], d['firstseen'], d['lastseen']) for d in result['archive_domains'])
+
+    except Exception as e:
+        if r.status_code == 429:
+            print('    - http status %s '% r.status_code)
+            sleep(2)
+            domains, archive_domains = check_dns(ns)
+
     return domains,archive_domains
 
 
 def show_dns(*args):
+
     ns = args[0]
-    domains, archive_domains = check_dns(ns)
+    domains, archive_domains = check_dns(ns.replace('\n', ' ').replace('\r', '').replace(' ', ''))
+
     if len(args) > 2:
         value = args[2]
-        print("\n".join([s for s in domains if value in str(s).lower()]))
-        print("\n".join([s for s in archive_domains if value in str(s).lower()]))
+        print("\n    - ".join([s for s in domains if value in str(s).lower()]))
+        print("\n    - ".join([s for s in archive_domains if value in str(s).lower()]))
     else:
-        print("\n".join([s for s in domains]))
-        print("\n".join([s for s in archive_domains]))
+        print("\n    - ".join([s for s in domains]))
+        print("\n    - ".join([s for s in archive_domains]))
 
 
 if __name__ == '__main__':
+    with open('nameservers.txt','r') as nameservers:
+        for ns in nameservers:
+            try:
+                print('  * %s' % ns)
+                show_dns(ns, *sys.argv)
+            except Exception as e:
+                print('    Error: %s' % e)
+                pass
 
     for z in get_zones():
         print(z['zone'])
         try:
             for ns in get_ns(z['zone']):
-                print(ns['name'])
-        except:
+                print('  * %s'%ns['name'])
+                show_dns(ns['name'],*sys.argv)
+        except Exception as e:
+            print('    Error: %s' % e)
             pass
 
-    '''
 
 
-    try:
-        show_dns('NS1.ANONS.IO',*sys.argv)
-    except Exception as e:
-        show_dns('NS2.ANONS.IO',*sys.argv)
-    try:
-        show_dns('NS75.DOMAINCONTROL.COM',*sys.argv)
-    except Exception as e:
-        show_dns('NS76.DOMAINCONTROL.COM',*sys.argv)
-    try:
-        show_dns('CARTER.NS.CLOUDFLARE.COM',*sys.argv)
-    except Exception as e:
-        show_dns('TINA.NS.CLOUDFLARE.COM',*sys.argv)
-'''
